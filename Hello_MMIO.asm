6628901193a43850f1baf04ac2db727e0f55014a
jbenaventeheras <j.benaventeheras@gmail.com>
2021-06-16 13:18:53 +0200

    añadido hola MMIO y readme con algunas instruccions

---
+++ Hello_MMIO.asm
@@ -0,0 +1,41 @@
+.data
+
+reciber_control_regis:	.word 0xFFFF0000
+keystroke_text_area:	.word 0xFFFF0004
+disp_ready_addr: 	.word 0xFFFF0008
+disp_register_addr:	.word 0xFFFF000C
+
+string:	.asciz "Hello MIMMIO SIMULATOR\n"
+
+.text
+setup:
+	# s0 <- display ready bit addr
+	# s1 <- display register addr
+	# s2 <- pointer to string
+	la	t0, disp_ready_addr
+	lw	s0, 0(t0)
+	la	t0, disp_register_addr
+	lw	s1, 0(t0)
+	la	s2, string
+	
+wait_for_ready:
+	# while(ready_bit == 0);
+	lw	t0, 0(s0)
+	beq	t0, zero, wait_for_ready
+	
+display_char:
+	# Check for null character
+	lb	t0, 0(s2)
+	beq	t0, zero, end_of_string
+	
+	# Write to display
+	sw	t0, 0(s1)
+	
+	# Increment string pointer
+	addi	s2, s2, 1
+	j	wait_for_ready
+	
+end_of_string:
+	# Terminate
+	li	a7, 10
+	ecall
