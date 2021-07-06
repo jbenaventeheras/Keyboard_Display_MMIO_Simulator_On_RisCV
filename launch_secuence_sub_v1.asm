2e38e00b9f2f39a38b9d22ec3f9f76cd3ea8bb9a
jbenaventeheras <j.benaventeheras@gmail.com>
2021-07-06 23:10:49 +0200

    first subrutine print_string done

--- launch_secuence_sub_v1.asm
+++ launch_secuence_sub_v1.asm
@@ -13,79 +13,51 @@ string:		.asciz "INTRODUZCA SECUENCIA DESPEGUE \n"
 string2:	.asciz "lANZAMIENTO INICIADO \n"
 
 .text
-setup:
+
 	# s0 <- display ready bit addr
 	# s1 <- display register addr
 	# s2 <- pointer to string
 	
-	li	s0, disp_ready_addr
-	li	s1, disp_register_addr
-	li 	s3, reciber_control_regis
-	li 	s4, keystroke_text_area
-	la	s2, string
+	li	a0, disp_ready_addr
+	li	a1, disp_register_addr
+	li 	a3, reciber_control_regis
+	li 	a4, keystroke_text_area
+	la	a2, string
 	
+#----------------------------------------------
+#-- PROGRAMA PRINCIPAL
+#----------------------------------------------
+	
+		jal print_string
+		
+	
+
+#-------------------------------------------------------------------	
+#------ SUBRUTINA: 
+#------   * Parametros de entrada: a0 disp_ready_addr a1 disp_register_addr a3 reciber_control_regis
+#------   * Parametros de salida: Ninguno
+#-------------------------------------------------------------------
+print_string:  #-- Punto de entrada
+
+
 wait_for_ready:
 	# while(ready_bit == 0);
-	lw	t0, 0(s0)
+	lw	t0, 0(a0)
 	beq	t0, zero, wait_for_ready
 	
 display_char:
 	# Check for null character
-	lb	t0, 0(s2)
-	beq	t0, zero, start_read
-	
-	# Write to display
-	sw	t0, 0(s1)
-	
-	# Increment string pointer
-	addi	s2, s2, 1
-	j	wait_for_ready
-	
-start_read:
-
-primerdigito:	
-		
-
-	
-	# esperar a que se ponga a 1 al introducir numero
-	lw	t1, 0(s3)
-	beq	t1, zero, start_read
-	
-	# comprobar primer digito clave
-	lb	t0, 0(s4)
-	li 	t1, primer_digito
-	bne     t0, t1, primerdigito
-	
-segundodigito:	
-		
-
-	
-	
-	# comprobar primer digito clave
-	lb	t0, 0(s4)
-	li 	t1, segundo_digito
-	bne     t0, t1, segundodigito
-
-
-	la	s2, string2
-wait_for_ready2:
-	# while(ready_bit == 0);
-	lw	t0, 0(s0)
-	beq	t0, zero, wait_for_ready2
-	
-display_char2:
-	# Check for null character
-	lb	t0, 0(s2)
+	lb	t0, 0(a2)
 	beq	t0, zero, end
 	
 	# Write to display
-	sw	t0, 0(s1)
+	sw	t0, 0(a1)
 	
 	# Increment string pointer
-	addi	s2, s2, 1
-	j	wait_for_ready2	
+	addi	a2, a2, 1
+	j	wait_for_ready
+	
+	
 
 end:
-	# Terminate
-	li	a7, 10
-	ecall
+	ret
