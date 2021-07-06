4076a78ee7ddfc69e03a8a883e0a587328321e69
jbenaventeheras <j.benaventeheras@gmail.com>
2021-07-06 23:35:38 +0200

    starting to respect the riscv convention assigned input of subrutines to a0-a7, and output to a0-a1

--- launch_secuence_sub_v1.asm
+++ launch_secuence_sub_v1.asm
@@ -18,23 +18,30 @@ string2:	.asciz "lANZAMIENTO INICIADO \n"
 	# s1 <- display register addr
 	# s2 <- pointer to string
 	
-	li	a0, disp_ready_addr
-	li	a1, disp_register_addr
-	li 	a3, reciber_control_regis
+	
 	li 	a4, keystroke_text_area
-	la	a2, string
+	la	a3, string
+	la	a5, string2
 	
 #----------------------------------------------
 #-- PROGRAMA PRINCIPAL
 #----------------------------------------------
-	
+		li	a0, disp_ready_addr
+		li	a1, disp_register_addr
+		li 	a2, reciber_control_regis
+		la	a3, string
 		jal print_string
 		
-	
+		
+		jal read_int
+		
+		# Terminate
+		li	a7, 10
+		ecall
 
 #-------------------------------------------------------------------	
 #------ SUBRUTINA: 
-#------   * Parametros de entrada: a0 disp_ready_addr a1 disp_register_addr a3 reciber_control_regis
+#------   * Parametros de entrada: a0 disp_ready_addr a1 disp_register_addr a2 reciber_control_regis a3 stringinput
 #------   * Parametros de salida: Ninguno
 #-------------------------------------------------------------------
 print_string:  #-- Punto de entrada
@@ -47,17 +54,34 @@ wait_for_ready:
 	
 display_char:
 	# Check for null character
-	lb	t0, 0(a2)
+	lb	t0, 0(a3)
 	beq	t0, zero, end
 	
 	# Write to display
 	sw	t0, 0(a1)
 	
 	# Increment string pointer
-	addi	a2, a2, 1
+	addi	a3, a3, 1
 	j	wait_for_ready
 	
 	
 
 end:
+	ret
+
+
+#-------------------------------------------------------------------	
+#------ SUBRUTINA: read
+#------   * Parametros de entrada: a0 disp_ready_addr a1 disp_register_addr a2 reciber_control_regis
+#------   * Parametros de salida: Ninguno
+#-------------------------------------------------------------------
+read_keyboard:  #-- Punto de entrada
+
+	
+	# esperar a que se ponga a 1 al introducir numero
+	lw	t1, 0(a2)
+	beq	t1, zero, read_keyboard
+	lb	t0, 0(s4)
+	mv 	t0, a0
+	
 	ret
